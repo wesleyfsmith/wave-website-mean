5c30a96bde5cfa88137e0060a5a0605fdec3fa11
Wesley Smith <wesleyfsmith@gmail.com>
2014-07-16 23:12:13 -0600

    Attemping to fix ngLocal issue

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'wave-website';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'textAngular'];
+	var applicationModuleVendorDependencies = ['ngLocale', 'ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'textAngular'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName) {
