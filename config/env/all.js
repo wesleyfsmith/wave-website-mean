6800d2613c58c66f3bbf6fd13064958a27a320a2
Wesley Smith <wesleyfsmith@gmail.com>
2014-08-17 17:52:28 -0600

    Changed home page text and switched to flatly theme.

--- config/env/all.js
+++ config/env/all.js
@@ -13,8 +13,8 @@ module.exports = {
 	assets: {
 		lib: {
 			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+//				'public/lib/bootstrap/dist/css/bootstrap.css',
+//				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
                 'public/lib/components-font-awesome/css/font-awesome.css'
 			],
 			js: [
