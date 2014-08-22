bf21067843e5cc59ee4d4bb23cb1f820fc040a78
Wesley Smith <wesleyfsmith@gmail.com>
2014-08-22 15:00:05 -0600

    Cleaned up bios client controller, delted teams, needs to be completely reworked.

--- public/modules/bios/controllers/bios.client.controller.js
+++ public/modules/bios/controllers/bios.client.controller.js
@@ -5,37 +5,8 @@ angular.module('bios').controller('BiosController', ['$scope', '$upload', '$stat
 	function($scope, $upload, $stateParams, $location, Authentication, Bios, Uploads, Teams ) {
 		$scope.authentication = Authentication;
 
-        $scope.photo = '';
-
         $scope.onFileSelect = function ($files){
             $scope.photo = $files[0];
-
-            //server side can handle anything!!!
-            //#serverswag
-//            $scope.upload = $upload.upload({
-//                url: '/uploads',
-//                method: 'POST',
-//                data: {},
-//                withCredentials: true,
-//                file: $scope.file
-//            }).success(function(data, status, headers, config) {
-//                $scope.photo = data.files[0].url;
-//            }).error(function(err){
-//                $scope.error = err.data.message;
-//            });
-
-
-
-        };
-
-        //initialize the bio teams
-
-        $scope.toggleTeamOnBio = function(team) {
-            if ($scope.arrayContains(team)) {
-                $scope.teams[team] = false;
-            } else {
-                $scope.teams[team] = true;
-            }
         };
 
         $scope.arrayContains = function(array, value) {
@@ -45,7 +16,7 @@ angular.module('bios').controller('BiosController', ['$scope', '$upload', '$stat
             return true;
         };
 
-        //logic to do when user clicks on bio
+        //logic to do when user clicks on bio MOVE TO DIRECTIVE
         $scope.selectBio = function(bio){
             //variable to keep track of currently clicked bio
             $scope.mouseOverBioId = bio._id;
@@ -65,8 +36,7 @@ angular.module('bios').controller('BiosController', ['$scope', '$upload', '$stat
 		$scope.create = function() {
             var bio = new Bios({
                 name: this.name,
-                title: this.title,
-                teams: $scope.teams
+                title: this.title
             });
 
             var errorFunction = function(errorResponse) {
@@ -120,33 +90,14 @@ angular.module('bios').controller('BiosController', ['$scope', '$upload', '$stat
 
         // Find a list of Bios
 		$scope.find = function() {
-
-
-//                .success(function(data) {
-//                for (var team in data) {
-//                    $scope.teams[team] = false;
-//                }
-//            });
-
-            //todo wesley document this!!!
 			$scope.bios = Bios.query();
-
-            //create 2d array for easier col/rows
 		};
 
 		// Find existing Bio
 		$scope.findOne = function() {
 			$scope.bio = Bios.get({ 
 				bioId: $stateParams.bioId
-			}).success(function(bio) {
-                //assign teams correctly
-                for (var team in $scope.teams) {
-                    if ($scope.arrayContains(bio.teams, team)) {
-                        $scope.teams[team] = true;
-                    }
-                }
-            });
-
+			});
 		};
 	}
 ]);
