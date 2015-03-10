14fdf06cf2fe7302d747a23e1adaf296e11c8e4f
Wesley Smith <wesleyfsmith@gmail.com>
2015-03-10 02:44:43 -0600

    Finished adding footer and fixed issue with tweets.

--- public/modules/core/directives/footer.client.directive.js
+++ public/modules/core/directives/footer.client.directive.js
@@ -1,20 +1,16 @@
 'use strict';
 
-angular.module('core').directive('footer', [
-	function() {
+angular.module('core').directive('footer', ['$http',
+	function($http) {
 		return {
 			templateUrl: '/modules/core/views/footer.client.view.html',
 			restrict: 'E',
-			link: function postLink($scope, element, attrs) {
-				// Footer directive logic
-				// ...
-
-				element.text('this is the footer directive');
-
-				$scope.tweets = [];
+			link: function postLink(scope, element, attrs) {
+				scope.tweets = [];
 
 				$http.get('/tweets').success(function(data, status, headers, config) {
-					$scope.tweets = data.statuses;
+					console.log('derp');
+					scope.tweets = data.statuses;
 				});
 			}
 		};
